88ae9b5311a2a5e908ade909ef3edc9ca5ec2af4
Barış İşbiliroğlu <barisisbiliroglu@gmail.com>
2023-01-18 17:48:51 +0300

    Added dequeue

--- STL/container/dequeue.cpp
+++ STL/container/dequeue.cpp
@@ -20,12 +20,12 @@ vectorün interface'i iledequeue interface'i hemen hemen aynı. sadece re-alloca
 
 
 +--------------------------------------------------------------------------------------------------------------------------------------------------+
-|                                                                                                                                                  |
+|                                                                                                                                                   |
 |                                                           std::dequeue Constructors                                                               |
-|                                                                                                                                                  |
+|                                                                                                                                                   |
 +==============================+=======================+===========================================================================================+
-| Constructor                  | Name                  | Explanation                                                                               |
-| ---------------------------- | --------------------- | ----------------------------------------------------------                                |
+| Constructor                  | Name                  | Explanation                                                                                |
+| ---------------------------- | --------------------- | ----------------------------------------------------------                                 |
 | dequeue<Elem> c              | Default constructor   | Creates an empty dequeue without any elements                                              |
 | dequeue<Elem> c(c2)          | Copy constructor      | Creates a new dequeue as a copy of c2 (all elements are copied)                            |
 | dequeue<Elem> c = c2         | Copy assignment       | Creates a new dequeue as a copy of c2 (all elements are copied)                            |
@@ -36,37 +36,48 @@ vectorün interface'i iledequeue interface'i hemen hemen aynı. sadece re-alloca
 | dequeue<Elem> c(beg,end)     |                       | Creates a dequeue initialized with the elements of the range[beg,end)                      |
 | dequeue<Elem> c(initlist)    |                       | Creates a dequeue initialized with the elements of initializer list initlist (since C++11) |
 | dequeue<Elem> c = initlist   |                       | Creates a dequeue initialized with the elements of initializer list initlist (since C++11) |
-| c.~dequeue()                 | Destructor            | Destroys all elements and frees the memory                                                |
+| c.~dequeue()                 | Destructor            | Destroys all elements and frees the memory                                                 |
 +------------------------------+-----------------------+-------------------------------------------------------------------------------------------+
 
 
 
 +===============================================================================================================================================================================================================================+
-|                                                  std::dequeue Inserting, removing and assign elements                                                                                                                                  |
+|                                                  std::dequeue Inserting, removing and assign elements                                                                                                                         |
 +=============================+=================================================================================================================================================================================================+
 | Function                    | Explanation                                                                                                                                                                                     |
 | --------------------------- | --------------------------------------                                                                                                                                                          |
-| c.push_back(elem)           | Appends a copy of elem at the end   
-| c.push_fron(elem)           | Appends a copy of elem at the begin                                                                                                                                                               |
+| c.push_back(elem)           | Appends a copy of elem at the end                                                                                                                                                               |
+| c.push_fron(elem)           | Appends a copy of elem at the begin                                                                                                                                                             |
 | c.pop_back()                | Removes the last element (does not return it)                                                                                                                                                   |
-| c.pop_front()               | Removes the first element (does not return it)                                                                                                                                                   |
+| c.pop_front()               | Removes the first element (does not return it)                                                                                                                                                  |
 | c.insert(pos,elem)          | Inserts a copy of elem before iterator position pos and returns the position of the new element                                                                                                 |
 | c.insert(pos,n,elem)        | Inserts n copies of elem before iterator position pos and returns the position of the first new element (or pos if there is no new element)                                                     |
 | c.insert(pos,beg,end)       | Inserts a copy of all elements of the range [beg,end) before iterator position pos and returns the position of the first newelement (or pos if there is no new element)                         |
 | c.insert(pos,initlist)      | Inserts a copy of all elements of the initializer list initlist before iterator position pos and returns the position of the first new element (or pos if there is no new element, since C++11) |
 | c.emplace(pos,args...)      | Inserts a copy of an element initialized with args before iterator position pos and returns the position of the new element (since C++11)                                                       |
 | c.emplace_back(args...)     | Appends a copy of an element initialized with args at the end (returns nothing, since C++11)                                                                                                    |
-| c.emplace_front(args...)    | Appends a copy of an element initialized with args at the begin (returns nothing, since C++11)                                                                                                    |
+| c.emplace_front(args...)    | Appends a copy of an element initialized with args at the begin (returns nothing, since C++11)                                                                                                  |
 | c.erase(pos)                | Removes the element at iterator position pos and returns the position of the next element                                                                                                       |
 | c.erase(beg,end)            | Removes all elements of the range [beg,end) and returns the position of the next element                                                                                                        |
 | c.resize(num)               | Changes the number of elements to num (if size() grows new elements are created by their default constructor)                                                                                   |
-| c.resize(num,elem)          | Changes the number of elements to num (if size() grows new elements are copies of elem)                                                                                                          |
-| c.clear()                   | Removes all elements (empties the container)   
-| c.assign(initlist)          | Assign a copy of all elements of the initializer list initlist                                                                                                               |
-| c.assign(n,elem)            | Assigns n copies of element elem                                                                                                              
-| c.assign(beg,end)           | Assigns the elements of the range [beg,end)                                                                                                              
+| c.resize(num,elem)          | Changes the number of elements to num (if size() grows new elements are copies of elem)                                                                                                         |
+| c.clear()                   | Removes all elements (empties the container)                                                                                                                                                    |
+| c.assign(initlist)          | Assign a copy of all elements of the initializer list initlist                                                                                                                                  |
+| c.assign(n,elem)            | Assigns n copies of element elem                                                                                                                                                                |
+| c.assign(beg,end)           | Assigns the elements of the range [beg,end)                                                                                                                                                     |
 +-----------------------------+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
 shrink_to_fit fonksiyonu: chunkları geri vermek için kullanılıyor. 
+
+
+Eğer ekleme iki uçtan yapılmıyorsa bütün iteratörler ve referanslar invalidate oluyor. Eğer ekleme iki u.tan birinden yapılıyorsa iteratörler invalidate oluyor ancak referanslar invalidate olmuyor. 
+Silme işlemi ortadan yapılıyorsa, bütün iteratörler ve referanslar invalidate oluyor. Eğer silme işlemi iki uçtan yapılıyorsa sadece silinen ögelerle iteratörler ve referanslar geçersiz hale geliyor. 
+
+vector bellekte ardışık. Dolayısıyla vectordeki ilk ögenin adresi C dizileri üzerinde işlem yapan bir fonksiyona geçilebilir. Ancak dequeue için bu geçerli değil. dequeue içerisinde chunklar kendisi içinde ardışık.
+
+vector<bool> diğer tipleri ile aynı specialization'a sahip değil. auto type deduction'da bazı problemlere yol açabiliyor. Bool yerine bir nested type tutuyor.
+dequeue<bool> ise gerçekten bool tutuyor. 
+
+dequeue stack container adaptöründe default container olarak kullanılıyor.  
 
 */
